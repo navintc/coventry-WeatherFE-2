6d36abee929f05b722b7839bb781fd389eb8d2a9
navintc <navin.thamindu@gmail.com>
2024-04-01 16:10:51 +0530

    Fixing AWS styling issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import "@/styles/globals.css";
+import '@aws-amplify/ui-react/styles.css';
+
 import {Amplify} from "aws-amplify";
 import awsconfig from "../aws-exports";
 Amplify.configure({...awsconfig, ssr: true})
